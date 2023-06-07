8ce52b84bc312fa6b4e830ae14135e88f754bb47
EPPR27 <113651234+EPPR27@users.noreply.github.com>
2023-06-07 08:46:39 -0500

    new file:   Ecommerce_Grupo5_React-main/.eslintrc.cjs 	new file:   Ecommerce_Grupo5_React-main/.gitignore 	new file:   Ecommerce_Grupo5_React-main/.prettierrc   new file:   Ecommerce_Grupo5_React-main/index.html 	new file:   Ecommerce_Grupo5_React-main/package.json 	new file:   Ecommerce_Grupo5_React-main/pnpm-lock.yaml 	new file:   Ecommerce_Grupo5_React-main/public/vite.svg 	new file:   Ecommerce_Grupo5_React-main/src/assets/react.svg 	new file:   Ecommerce_Grupo5_React-main/src/main.jsx 	new file:   Ecommerce_Grupo5_React-main/src/pages/HomePage.jsx 	new file:   Ecommerce_Grupo5_React-main/src/router.jsx 	new file:   Ecommerce_Grupo5_React-main/vite.config.js

---
+++ Ecommerce_Grupo5_React-main/src/main.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+import { RouterProvider } from 'react-router-dom'
+
+import router from './router'
+
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query'
+
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+const client = new QueryClient()
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <QueryClientProvider client={client}>
+      <RouterProvider router={router} />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  </React.StrictMode>
+)
